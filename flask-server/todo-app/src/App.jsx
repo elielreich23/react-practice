0dba44e161295583b3230de0d2cc3c7536bbefb2
Yemi Oduyemi <yhermii@gmail.com>
2023-05-16 20:17:48 +0100

    Initial Commit. Project setup

---
+++ flask-server/todo-app/src/App.jsx
@@ -0,0 +1,32 @@
+import './App.scss';
+import Sidebar from './components/sidebar/Sidebar';
+import Todo from './components/Todo-section/Todo';
+import Progress from './components/Progress-section/Progress';
+import Done from './components/Done-section/Done';
+import "./components/FontAwesome"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <>
+      {/* <div className="app">
+have to edit it 
+    </div> */}
+      <Sidebar />
+      <Todo/>
+      <Progress/>
+      <Done/>
+      {/* <Progress/> */}
+      <BrowserRouter>
+        <Routes>
+          {/* link to / */}
+
+          <Route path="../src/Pages/profile/index.jsx" element={<Sidebar />} />
+
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ flask-server/todo-app/src/components/header/Header.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import './style.scss'
+import { AiOutlineCalendar } from "react-icons/ai"
+import { IoSearch } from "react-icons/io5"
+import { FaRegBell } from "react-icons/fa"
+
+
+const Header = () => {
+  return (
+    <>
+      <div className="header-container">
+        <div className="header-content">
+          <div className="text">Welcome back, Vincent <span> 👋</span>
+            {/* create a function to collect user name upon signup */}
+          </div>
+          <div className="header-icons">
+            <IoSearch />
+            <FaRegBell />
+            <AiOutlineCalendar />
+            <span>
+              {/* this will be change to a dynamic date picked from  */}
+              19 May 2022
+            </span>
+          </div>
+          <div className="profile">
+            <img src="../../assets/Image.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="subheader">
+        <div className="right">
+          {/* <img src="./1.png" alt="" /> */}
+          <div className="board">
+            <div className="rec">
+            <p className="one"></p>
+            <p className="two" ></p>
+            </div>
+            {/* <img src="" alt="" /> */}
+            <h4>Board view</h4>
+          </div>
+
+          <div className="add-view">
+            {/* adjust plus sign in circle and create logic for adding new wiew */}
+            <div className="circle">
+              <span>+</span>
+            </div>
+            <h3>Add view</h3>
+          </div>
+        </div>
+        <div className="left">
+          <h5 className="bold" >Filter</h5>
+          <h5>
+            <span>Sort</span>
+          </h5>
+          <div className="dots-container"></div>
+          <p>...</p>
+          <button>
+            <span>New Template</span>
+          </button>
+        </div>
+        <div className="line"></div>
+      </div>
+    </>
+  )
+}
+
+export default Header
