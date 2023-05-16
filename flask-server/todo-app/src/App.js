a30bf191921a9c019a68f4c8bc292b8a6f2d631b
Yemi Oduyemi <yhermii@gmail.com>
2023-05-16 20:51:46 +0100

    Installing dependencies to get started

--- flask-server/todo-app/src/App.js
+++ flask-server/todo-app/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Sidebar from './components/sidebar/Sidebar';
+import Todo from './components/Todo-section/Todo';
+import Progress from './components/Progress-section/Progress';
+import Done from './components/Done-section/Done';
+import "./components/FontAwesome"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <div className="app">
+have to edit it 
+    </div> */}
+      <Sidebar />
+      <Todo/>
+      <Progress/>
+      <Done/>
+      {/* <Progress/> */}
+      <BrowserRouter>
+        <Routes>
+          {/* link to / */}
+
+          <Route path="../src/Pages/profile/index.jsx" element={<Sidebar />} />
+
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
